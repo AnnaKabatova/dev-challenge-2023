dd205a1bfa15bd0741ddc15d3bed58a556d9428a
Anna Kabatova <kabatova.anna@ukr.net>
2023-10-01 21:23:33 +0300

    new models layout

--- sheets/models.py
+++ sheets/models.py
@@ -1,22 +1,15 @@
 from django.db import models
 
 
-class Sheet(models.Model):
-    sheet_id = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.sheet_id
+class Spreadsheet(models.Model):
+    id = models.CharField(primary_key=True, max_length=30)
 
 
 class Cell(models.Model):
-    cell_id = models.CharField(max_length=255)
-    sheet = models.ForeignKey(
-        Sheet,
-        on_delete=models.CASCADE,
-        related_name='cell',
-    )
+    cell_id = models.CharField(max_length=30)
     value = models.CharField(max_length=255)
-    result = models.CharField(max_length=255, null=True)
-
-    def __str__(self):
-        return f'{self.sheet.sheet_id}/{self.cell_id}'
+    result = models.CharField(max_length=255)
+    spreadsheet = models.ForeignKey(Spreadsheet, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = (("spreadsheet", "cell_id"),)
