1acb4353be4ddff2639d3d75bd2d6c4f8ba98aa2
Anna Kabatova <kabatova.anna@ukr.net>
2023-10-02 11:29:41 +0300

    made views restricted to only authenticated users

--- sheets/views.py
+++ sheets/views.py
@@ -1,9 +1,8 @@
 import re
-
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated
 from .models import Spreadsheet, Cell
 from .serializers import CellSerializer
 
@@ -36,6 +35,7 @@ def calculate_formula(formula, spreadsheet):
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def cell_create_retrieve(request, sheet_id, cell_id):
     try:
         spreadsheet, created = Spreadsheet.objects.get_or_create(id=sheet_id)
@@ -71,8 +71,8 @@ def cell_create_retrieve(request, sheet_id, cell_id):
     else:
         return Response({'error': 'Unsupported HTTP method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def sheet_data(request, sheet_id):
     try:
         spreadsheet = Spreadsheet.get_by_id(id=sheet_id)
