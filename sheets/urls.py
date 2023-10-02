65620fe1a523f6520eff23b30333cd0954162edf
Anna Kabatova <kabatova.anna@ukr.net>
2023-10-02 11:29:53 +0300

    added jwt to use

--- sheets/urls.py
+++ sheets/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from .views import cell_create_retrieve, sheet_data
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('<sheet_id>/<cell_id>/', cell_create_retrieve),
-    path('<sheet_id>/', sheet_data),
+    path('<sheet_id>/<cell_id>/', cell_create_retrieve, name='cell-create-retrieve'),
+    path('<sheet_id>/', sheet_data, name='sheet-data'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 app_name = 'sheets'
