40b717a7ad7f30c44443ba480a7e65ba8bab2601
Anna Kabatova <kabatova.anna@ukr.net>
2023-10-02 13:56:20 +0300

    fix for user

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.settings import api_settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -14,6 +14,7 @@ class CreateTokenView(ObtainAuthToken):
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
