194f88a0dbe8ba1d97c77d7bcff4b7f8e026d53f
Anna Kabatova <kabatova.anna@ukr.net>
2023-10-02 12:01:50 +0300

    added admin

--- sheets/admin.py
+++ sheets/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Spreadsheet, Cell
 
-# Register your models here.
+
+@admin.register(Spreadsheet)
+class SpreadsheetAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+@admin.register(Cell)
+class CellAdmin(admin.ModelAdmin):
+    list_display = ('cell_id', 'value', 'result', 'spreadsheet')
+    list_filter = ('spreadsheet',)
